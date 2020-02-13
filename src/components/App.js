0b7c313564b2e67c1d051d951a18be6e2ba95765
Alex Shynkarenko <shynkarenkoalex5@gmail.com>
2020-02-13 22:28:07 +0200

    section

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
-import Button from './Button';
+import Section from './Section';
 
 export default class App extends Component {
 
@@ -30,21 +30,11 @@ export default class App extends Component {
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
     const keys = Object.keys(this.state);
-    return (
-      <div>
-          <h1>Please leave feedback</h1>
-          <FeedbackOptions options={keys} onLeaveFeedback={this.handleChangeStatistics}/>
-
-          {/*{keys.map(key => (*/}
-          {/*    <Button keys={key} title={key} onChangeStatistics={() => this.handleChangeStatistics({key})} />*/}
-          {/*))}*/}
-
-          {/*<Button title='Good' onChangeStatistics={() => this.handleChangeStatistics('good')} />*/}
-          {/*<Button title='Neutral' onChangeStatistics={() => this.handleChangeStatistics('neutral')} />*/}
-          {/*<Button title='Bad' onChangeStatistics={() => this.handleChangeStatistics('bad')} />*/}
-          <h2>Statistics</h2>
-          <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/>                    
-      </div>
+    return (          
+          <Section title = 'Statistics' header = 'Please leave feedback'>
+            <FeedbackOptions options={keys} onLeaveFeedback={this.handleChangeStatistics}/>
+            <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/>
+          </Section>
     )        
   }
 }
